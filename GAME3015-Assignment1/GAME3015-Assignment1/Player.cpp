f70324ae032dff2a32f9d024ffa759c4dc80ec2a
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2022-03-18 16:56:29 -0400

    Added Sudo Code to player from Week 9-10
    Pretty sure we will need enum actions with move functions as bare minimum.
    
    Use Keybind code as well.
    
    Not sure we need: Mission Status, launch Missile, and Firing.

--- GAME3015-Assignment1/GAME3015-Assignment1/Player.cpp
+++ GAME3015-Assignment1/GAME3015-Assignment1/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+//Assignment SUDO 
 //The Player class contains two methods to react to the eventsand real - time input, respectively :
 //	class Player
 //{
@@ -26,3 +27,59 @@
 //		commands.push(moveLeft);
 //	}
 //}
+
+//Referance Sudo 
+
+//#include <Command.hpp>
+//
+//#include <SFML/Window/Event.hpp>
+//
+//#include <map>
+//
+//
+//class CommandQueue;
+//
+//class Player
+//{
+//public:
+//	enum Action
+//	{
+//		MoveLeft,
+//		MoveRight,
+//		MoveUp,
+//		MoveDown,
+//		Fire,
+//		LaunchMissile,
+//		ActionCount
+//	};
+//
+//	enum MissionStatus
+//	{
+//		MissionRunning,
+//		MissionSuccess,
+//		MissionFailure
+//	};
+//
+//
+//public:
+//	Player();
+//
+//	void					handleEvent(const sf::Event& event, CommandQueue& commands);
+//	void					handleRealtimeInput(CommandQueue& commands);
+//
+//	void					assignKey(Action action, sf::Keyboard::Key key);
+//	sf::Keyboard::Key		getAssignedKey(Action action) const;
+//
+//	void 					setMissionStatus(MissionStatus status);
+//	MissionStatus 			getMissionStatus() const;
+//
+//private:
+//	void					initializeActions();
+//	static bool				isRealtimeAction(Action action);
+//
+//
+//private:
+//	std::map<sf::Keyboard::Key, Action>		mKeyBinding;
+//	std::map<Action, Command>				mActionBinding;
+//	MissionStatus 							mCurrentMissionStatus;
+//};
